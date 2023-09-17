8baac686615698aa4834b9ac318899e48eff6d57
Dan-k391 <wang.yifan@ulink.cn>
2023-09-17 21:02:43 +0800

    ...

--- README.md
+++ README.md
@@ -12,12 +12,66 @@ still in progress
 
 ### Usage
 
+Use `runCode(inputCode, outputFunction)`
 ```js
 import * as promise from "pseudo2wasm";
 
-let pseudo2wasm;
-promise.then((foo) => pseudo2wasm = foo);
+let pseudo2wasm = await promise;
 
 pseudo2wasm.runCode(`OUTPUT 1`, console.log)
+```
+OR construct your own compiler
+```js
+function compile(input) {
+    if (!input) {
+        return;
+    }
+    const scanner = new pseudo2wasm.Scanner(input);
+    const tokens = scanner.scan();
+    console.log(tokens);
+    const parser = new pseudo2wasm.Parser(tokens);
+    const ast = parser.parse();
+    console.log(ast);
+    const generator = new pseudo2wasm.Generator(ast);
+    const module = generator.generate();
+    return module;
+}
 
+async function runtime(input, output) {
+    const module = compile(input);
+
+    // module.optimize();
+
+    console.log(module.emitText());
+    const wasm = module.emitBinary();
+
+    // initialize import objects
+    const memory = new WebAssembly.Memory({ initial: 1, maximum: 2 });
+    const importObect = {
+        env: {
+            buffer: memory,
+            logInteger: (value) => {
+                output(value);
+            },
+            logReal: (value) => {
+                console.log(value);
+            },
+            logChar: (value) => {
+                console.log(String.fromCharCode(value));
+            },
+            logString: (value) => {
+                const bytes = new Uint8Array(memory.buffer, value);
+                const str = bytes.toString();
+                console.log(str);
+            }
+        },
+    }
+    const { instance } = await WebAssembly.instantiate(wasm, importObect);
+
+    const main = instance.exports.main;
+    const start = new Date().getTime();
+    main();
+    const end = new Date().getTime();
+    return end - start;
+}
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,302 +1,366 @@
 // TODO: add ts type to every statement
 
-import binaryen from "binaryen";
 import { Compiler } from "./compiler";
+import { Scanner } from "./scanning/scanner";
+import { Parser } from "./parsing/parser";
+import { Generator } from "./codegenerating/generator";
 
 
-const code0 = `OUTPUT 1 + 9
-`
+// const code0 = `OUTPUT 1 + 9
+// `;
 
-const code1 = `DECLARE i:INTEGER
-DECLARE j:INTEGER
-DECLARE k:REAL
+// const code1 = `DECLARE i:INTEGER
+// DECLARE j:INTEGER
+// DECLARE k:REAL
 
-i <- 1
-j <- 2
-k <- 3.14
-OUTPUT i + j * k
-`
+// i <- 1
+// j <- 2
+// k <- 3.14
+// OUTPUT i + j * k
+// `;
 
-const code2 = `DECLARE i:INTEGER
-DECLARE j:INTEGER
+// const code2 = `DECLARE i:INTEGER
+// DECLARE j:INTEGER
 
-i <- -1.99
-j <- 2.99
-IF i = j + i THEN
-    OUTPUT 1
-ELSE
-    OUTPUT 2
-ENDIF
-`
+// i <- -1.99
+// j <- 2.99
+// IF i = j + i THEN
+//     OUTPUT 1
+// ELSE
+//     OUTPUT 2
+// ENDIF
+// `;
 
-const code3 = `DECLARE i:STRING
-i <- "Hi"
-OUTPUT i
-`
+// const code3 = `DECLARE i:STRING
+// i <- "Hi"
+// OUTPUT i
+// `;
 
-const code4 = `DECLARE i:INTEGER
-DECLARE j:INTEGER
+// const code4 = `DECLARE i:INTEGER
+// DECLARE j:INTEGER
 
-i <- 1
-WHILE i < 10
-    i <- i + 1
-    j <- i
-ENDWHILE
+// i <- 1
+// WHILE i < 10
+//     i <- i + 1
+//     j <- i
+// ENDWHILE
 
-OUTPUT j
-`
+// OUTPUT j
+// `;
 
-const code5 = `DECLARE i:INTEGER
-DECLARE j:INTEGER
+// const code5 = `DECLARE i:INTEGER
+// DECLARE j:INTEGER
 
-i <- 1
-REPEAT
-    i <- i + 1
-    j <- i
-UNTIL i = 10
+// i <- 1
+// REPEAT
+//     i <- i + 1
+//     j <- i
+// UNTIL i = 10
 
-OUTPUT j
-`
+// OUTPUT j
+// `;
 
-const code6 = `DECLARE i:INTEGER
-DECLARE j:INTEGER
+// const code6 = `DECLARE i:INTEGER
+// DECLARE j:INTEGER
 
-FOR i <- 1 TO 11
-    j <- i
-NEXT i
+// FOR i <- 1 TO 11
+//     j <- i
+// NEXT i
 
-OUTPUT j
-`
+// OUTPUT j
+// `;
 
-const code7 = `FUNCTION add(a:INTEGER, b:INTEGER) RETURNS INTEGER
-    RETURN a + b
-ENDFUNCTION
+// const code7 = `FUNCTION add(a:INTEGER, b:INTEGER) RETURNS INTEGER
+//     RETURN a + b
+// ENDFUNCTION
 
-DECLARE i:INTEGER
-DECLARE j:INTEGER
-
-i <- 1
-j <- 2
-OUTPUT add(i, j)
-`
-
-const code8 = `FUNCTION for(a:INTEGER) RETURNS INTEGER
-    DECLARE b:INTEGER
-    DECLARE c:INTEGER
-
-    FOR b <- 1 TO a
-        c <- b
-    NEXT b
-    RETURN c
-ENDFUNCTION
-
-OUTPUT for(10)
-`
-
-const code9 = `FUNCTION for(a:INTEGER) RETURNS INTEGER
-    DECLARE b:INTEGER
-
-    FOR a <- 1 TO 11
-        b <- a
-    NEXT a
-    RETURN b
-ENDFUNCTION
-
-OUTPUT for(1)
-`
-
-const code10 = `FUNCTION recur(a:INTEGER) RETURNS INTEGER
-    IF a = 10 THEN
-        RETURN a
-    ENDIF
-    RETURN recur(a + 1)
-ENDFUNCTION
-
-OUTPUT recur(1)
-`
-
-const code11 = `DECLARE a:INTEGER
-a <- 1
-
-FUNCTION scope() RETURNS INTEGER
-    DECLARE b:INTEGER
-    DECLARE c:INTEGER
-
-    b <- 1
-    c <- 2
-    RETURN a + b + c
-ENDFUNCTION
-
-OUTPUT scope()
-`
-
-const code12 = `PROCEDURE print(a:INTEGER)
-    DECLARE b:INTEGER
-    DECLARE c:INTEGER
-
-    b <- 2
-    c <- 2
-    OUTPUT a + b + c
-ENDPROCEDURE
-
-CALL print(1)
-`
-
-const code13 = `DECLARE i: INTEGER
-i <- 1
-
-PROCEDURE increment(BYREF a:INTEGER)
-    a <- a + 1
-ENDPROCEDURE
-
-CALL increment(i)
-OUTPUT i
-`
-
-const code14 = `DECLARE i: INTEGER
-i <- 1
-
-PROCEDURE increment()
-    i <- i + 1
-ENDPROCEDURE
-
-CALL increment()
-OUTPUT i
-`
-
-const code15 = `FUNCTION add(a:REAL, b:REAL) RETURNS REAL
-    DECLARE c: REAL
-    c <- a + b
-    RETURN c
-ENDFUNCTION
-
-DECLARE i: REAL
-DECLARE j: REAL
-
-i <- 1.1
-j <- 2.2
-OUTPUT add(i, j)
-`
-
-const code16 = `OUTPUT 'a'`
-
-const code17 = `DECLARE i: CHAR
-i <- 'v'
-
-OUTPUT i
-`
-
-const code18 = `DECLARE i: INTEGER
-
-i <- 1
-OUTPUT -i
-`
-
-const code19 = `DECLARE i: REAL
-
-i <- 1
-OUTPUT -i
-`
-
-
-// const code11 = `TYPE a = ^INTEGER
 // DECLARE i:INTEGER
+// DECLARE j:INTEGER
 
-// i <- 9
-// a <- ^i
-// OUTPUT a^
-// `
+// i <- 1
+// j <- 2
+// OUTPUT add(i, j)
+// `;
 
-// const code12 = `TYPE a = ^INTEGER
-// DECLARE i:INTEGER
+// const code8 = `FUNCTION for(a:INTEGER) RETURNS INTEGER
+//     DECLARE b:INTEGER
+//     DECLARE c:INTEGER
 
-// i <- 9
-// a <- ^^^^i^^^
-// OUTPUT a^
-// `
+//     FOR b <- 1 TO a
+//         c <- b
+//     NEXT b
+//     RETURN c
+// ENDFUNCTION
 
-const codes = [
-    code0,
-    code1,
-    code2,
-    code3,
-    code4,
-    code5,
-    code6,
-    code7,
-    code8,
-    code9,
-    code10,
-    code11,
-    code12,
-    code13,
-    code14,
-    code15,
-    code16,
-    code17,
-    code18,
-    code19
-];
-const expected = [10, 7.28, 2, 9/*"Hi"*/, 10, 10, 11, 3, 10, 11, 10, 4, 5, 2, 2, 1.1+2.2/*floating point inaccuracy*/
-    , 'a', 'v', -1, -1];
-let total = codes.length;
-let compileCount = 0;
-let runCount = 0;
-const compileFailed: Array<number> = [];
-const runFailed: Array<number> = [];
+// OUTPUT for(10)
+// `;
 
-async function compileTest() {
-    for (let i = 0; i < total; i++) {
-        console.log(i);
-        console.log(codes[i]);
-        const compiler = new Compiler(codes[i]);
-        try {
-            compiler.compile();
-            compileCount++;
-        } 
-        catch (e) {
-            console.log(e);
-            compileFailed.push(i);
-        }
+// const code9 = `FUNCTION for(a:INTEGER) RETURNS INTEGER
+//     DECLARE b:INTEGER
 
-        try {
-            // if fail push
-            if (await compiler.test(expected[i]) === false)
-                runFailed.push(i);
-            else
-                runCount++;
-        }
-        catch (e) {
-            console.log(e);
-            runFailed.push(i);
-        }
-    }
-}
+//     FOR a <- 1 TO 11
+//         b <- a
+//     NEXT a
+//     RETURN b
+// ENDFUNCTION
 
+// OUTPUT for(1)
+// `;
+
+// const code10 = `FUNCTION recur(a:INTEGER) RETURNS INTEGER
+//     IF a = 10 THEN
+//         RETURN a
+//     ENDIF
+//     RETURN recur(a + 1)
+// ENDFUNCTION
+
+// OUTPUT recur(1)
+// `;
+
+// const code11 = `DECLARE a:INTEGER
+// a <- 1
+
+// FUNCTION scope() RETURNS INTEGER
+//     DECLARE b:INTEGER
+//     DECLARE c:INTEGER
+
+//     b <- 1
+//     c <- 2
+//     RETURN a + b + c
+// ENDFUNCTION
+
+// OUTPUT scope()
+// `;
+
+// const code12 = `PROCEDURE print(a:INTEGER)
+//     DECLARE b:INTEGER
+//     DECLARE c:INTEGER
+
+//     b <- 2
+//     c <- 2
+//     OUTPUT a + b + c
+// ENDPROCEDURE
+
+// CALL print(1)
+// `;
+
+// const code13 = `DECLARE i: INTEGER
+// i <- 1
+
+// PROCEDURE increment(BYREF a:INTEGER)
+//     a <- a + 1
+// ENDPROCEDURE
+
+// CALL increment(i)
+// OUTPUT i
+// `;
+
+// const code14 = `DECLARE i: INTEGER
+// i <- 1
+
+// PROCEDURE increment()
+//     i <- i + 1
+// ENDPROCEDURE
+
+// CALL increment()
+// OUTPUT i
+// `;
+
+// const code15 = `FUNCTION add(a:REAL, b:REAL) RETURNS REAL
+//     DECLARE c: REAL
+//     c <- a + b
+//     RETURN c
+// ENDFUNCTION
+
+// DECLARE i: REAL
+// DECLARE j: REAL
+
+// i <- 1.1
+// j <- 2.2
+// OUTPUT add(i, j)
+// `;
+
+// const code16 = `OUTPUT 'a'`;
+
+// const code17 = `DECLARE i: CHAR
+// i <- 'v'
+
+// OUTPUT i
+// `;
+
+// const code18 = `DECLARE i: INTEGER
+
+// i <- 1
+// OUTPUT -i
+// `;
+
+// const code19 = `DECLARE i: REAL
+
+// i <- 1
+// OUTPUT -i
+// `;
+
+// // const code11 = `TYPE a = ^INTEGER
+// // DECLARE i:INTEGER
+
+// // i <- 9
+// // a <- ^i
+// // OUTPUT a^
+// // `
+
+// // const code12 = `TYPE a = ^INTEGER
+// // DECLARE i:INTEGER
+
+// // i <- 9
+// // a <- ^^^^i^^^
+// // OUTPUT a^
+// // `
+
+// const codes = [
+//     code0,
+//     code1,
+//     code2,
+//     code3,
+//     code4,
+//     code5,
+//     code6,
+//     code7,
+//     code8,
+//     code9,
+//     code10,
+//     code11,
+//     code12,
+//     code13,
+//     code14,
+//     code15,
+//     code16,
+//     code17,
+//     code18,
+//     code19,
+// ];
+// const expected = [
+//     10,
+//     7.28,
+//     2,
+//     9 /*"Hi"*/,
+//     10,
+//     10,
+//     11,
+//     3,
+//     10,
+//     11,
+//     10,
+//     4,
+//     5,
+//     2,
+//     2,
+//     1.1 + 2.2 /*floating point inaccuracy*/,
+//     "a",
+//     "v",
+//     -1,
+//     -1,
+// ];
+// let total = codes.length;
+// let compileCount = 0;
+// let runCount = 0;
+// const compileFailed: Array<number> = [];
+// const runFailed: Array<number> = [];
+
+// async function compileTest() {
+//     for (let i = 0; i < total; i++) {
+//         console.log(i);
+//         console.log(codes[i]);
+//         const compiler = new Compiler(codes[i]);
+//         try {
+//             compiler.compile();
+//             compileCount++;
+//         } catch (e) {
+//             console.log(e);
+//             compileFailed.push(i);
+//         }
+
+//         try {
+//             // if fail push
+//             if ((await compiler.test(expected[i])) === false) runFailed.push(i);
+//             else runCount++;
+//         } catch (e) {
+//             console.log(e);
+//             runFailed.push(i);
+//         }
+//     }
+// }
 
 // The interpreter part now is under the interpreter branch, and it is not finished
 
 // compileTest().then(() => {
-    // fetch("http://127.0.0.1:5570/wasmtry/pointer.wasm")
-    // .then((response) => response.arrayBuffer())
-    // .then((buffer) => new Uint8Array(buffer))
-    // .then((array) => binaryen.readBinary(array))
-    // .then((module) => {console.log(module.emitText())});
+// fetch("http://127.0.0.1:5570/wasmtry/pointer.wasm")
+// .then((response) => response.arrayBuffer())
+// .then((buffer) => new Uint8Array(buffer))
+// .then((array) => binaryen.readBinary(array))
+// .then((module) => {console.log(module.emitText())});
 
-    // const instance = new WebAssembly.Instance(module);
+// const instance = new WebAssembly.Instance(module);
 //     console.log(`compileCount: ${compileCount}/${total}`);
 //     console.log(`compileFailed: ${compileFailed}`);
 //     console.log(`runCount: ${runCount}/${total}`);
 //     console.log(`runFailed: ${runFailed}`);
 // });
 
-export async function runCode(code: string, output: (a: number) => void): Promise<number> {
+export async function runCode(
+    code: string,
+    output: (a: number) => void
+): Promise<number> {
     const compiler = new Compiler(code);
     const time = await compiler.runtime(output);
 
     return time;
 }
 
-export function testCompiler(): number {
-    return 2;
-}
+export const keyWords = [
+    "FUNCTION",
+    "ENDFUNCTION",
+    "PROCEDURE",
+    "ENDPROCEDURE",
+    "BYVAL",
+    "BYREF",
+    "RETURNS",
+    "RETURN",
+    "CALL",
+    "DECLARE",
+    "ARRAY",
+    "OF",
+    "TYPE",
+    "ENDTYPE",
+    "IF",
+    "THEN",
+    "ELSE",
+    "ENDIF",
+    "WHILE",
+    "ENDWHILE",
+    "REPEAT",
+    "UNTIL",
+    "FOR",
+    "TO",
+    "STEP",
+    "NEXT",
+    "MOD",
+    "AND",
+    "OR",
+    "NOT",
+    "OUTPUT",
+    "INPUT",
+    "RND",
+    "TIME",
+    "TRUE",
+    "FALSE",
+    "INTEGER",
+    "REAL",
+    "CHAR",
+    "STRING",
+    "BOOLEAN",
+];
+
+export { Scanner };
+export { Parser };
+export { Generator };
+
